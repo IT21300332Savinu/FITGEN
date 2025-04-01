b4a2f0522e5a789c5e3f3344ef4effec61806263
it21809842 <wasanajayamali01@gmail.com>
2025-04-01 16:14:17 +0530

    updated health_dashboard

--- medical_guidance_system/lib/views/health_dashboard.dart
+++ medical_guidance_system/lib/views/health_dashboard.dart
@@ -16,6 +16,17 @@ class HealthDashboard extends StatelessWidget {
               "Health Metrics",
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(height: 40),
+            Card(
+              child: ListTile(
+                title: const Text("Halth Status"),
+                subtitle: const Text("Normal"),
+                trailing: Icon(
+                  Icons.favorite,
+                  color: const Color.fromARGB(255, 6, 179, 130),
+                ),
+              ),
+            ),
             const SizedBox(height: 10),
             Card(
               child: ListTile(
@@ -24,12 +35,40 @@ class HealthDashboard extends StatelessWidget {
                 trailing: Icon(Icons.bloodtype, color: Colors.red[400]),
               ),
             ),
+
             const SizedBox(height: 10),
             Card(
               child: ListTile(
                 title: const Text("Heart Rate"),
                 subtitle: const Text("72 BPM"),
                 trailing: Icon(Icons.favorite, color: Colors.pink[300]),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Card(
+              child: ListTile(
+                title: const Text("BP"),
+                subtitle: const Text("72 BPM"),
+                trailing: Icon(Icons.favorite, color: Colors.pink[300]),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Card(
+              child: ListTile(
+                title: const Text("oxygen Level"),
+                subtitle: const Text("72 BPM"),
+                trailing: Icon(Icons.favorite, color: Colors.pink[300]),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Card(
+              child: ListTile(
+                title: const Text("Recomndation Workout"),
+                subtitle: const Text("dsffaaaaaaaaaaaas"),
+                trailing: Icon(
+                  Icons.favorite,
+                  color: const Color.fromARGB(255, 98, 193, 240),
+                ),
               ),
             ),
             const SizedBox(height: 10),
