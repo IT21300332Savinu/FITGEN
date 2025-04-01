c9b6466ef763b6b4633ecb6106f2e9a7ccd24c57
it21809842 <wasanajayamali01@gmail.com>
2025-04-01 14:08:01 +0530

    updated main.dart

--- medical_guidance_system/lib/main.dart
+++ medical_guidance_system/lib/main.dart
@@ -1,37 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'views/home_screen.dart';
 import 'views/health_dashboard.dart';
 import 'views/workout_planner.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
-
-final GoRouter _router = GoRouter(
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
-    GoRoute(
-      path: '/dashboard',
-      builder: (context, state) => const HealthDashboard(),
-    ),
-    GoRoute(
-      path: '/workout',
-      builder: (context, state) => const WorkoutPlanner(),
-    ),
-  ],
-);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AI Medical Guidance',
       theme: ThemeData(primarySwatch: Colors.blue),
-      routerConfig: _router, // Use GoRouter
+      initialRoute: '/', // Home Screen
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/dashboard': (context) => const HealthDashboard(),
+        '/workout': (context) => const WorkoutPlanner(),
+      },
     );
   }
 }
